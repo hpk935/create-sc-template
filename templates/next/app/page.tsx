83c3c220060e3ba89be54b8f8e0a86dfc938dfc0
hpk935 <83901776+hpk935@users.noreply.github.com>
2026-03-23 23:26:23 -0700

    tehe

--- templates/next/app/page.tsx
+++ templates/next/app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello hehe
+     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p className="text-lg font-medium text-gray-900 dark:text-white">
+        Hello hehe
+      </p>
     </div>
   );
 }
